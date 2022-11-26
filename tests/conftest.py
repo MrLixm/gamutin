e2a0db7703b13d3198b9760fca630d96a62ff5d9
Liam Collod <lcollod@gmail.com>
2022-11-26 20:23:28 +0100

    [tests] added logging config to conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+import logging.config
+
 import pytest
 from pathlib import Path
 
@@ -18,3 +20,41 @@ def imagepath_color_bars_alpha():
 @pytest.fixture
 def imagepath_wheel_mchannel():
     return DATA_DIR / "wheel_mchannel.exr"
+
+
+def _configureLogging():
+    """
+    Configure the python logging module
+    """
+
+    logging_config = {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "formatters": {
+            "fmt_standard": {
+                "format": "{levelname: <7} | {asctime} |[{name: >30}]{message}",
+                "style": "{",
+            }
+        },
+        "handlers": {
+            "hl_console": {
+                "level": "DEBUG",
+                "formatter": "fmt_standard",
+                "class": "logging.StreamHandler",
+                "stream": "ext://sys.stdout",
+            },
+        },
+        "loggers": {
+            f"": {
+                "handlers": ["hl_console"],
+                "level": logging.DEBUG,
+                "propagate": False,
+            },
+        },
+    }
+    # register
+    logging.config.dictConfig(logging_config)
+    return
+
+
+_configureLogging()
