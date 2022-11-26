95cc068e08b5c21665c2500e4739069368a443c2
Liam Collod <lcollod@gmail.com>
2022-11-26 18:45:09 +0100

    [core.io] added channels handling

--- pgcheck/core/io.py
+++ pgcheck/core/io.py
@@ -29,11 +29,44 @@ class ImageRead:
     path: Path
     colorspace: Optional[colour.RGB_Colourspace]
 
-    def get_image_buf(self, subimage: int = 0, mipmap: int = 0) -> oiio.ImageBuf:
-        return oiio.ImageBuf(str(self.path), subimage, mipmap)
+    def get_image_buf(
+        self,
+        channels: Optional[tuple] = None,
+        subimage: int = 0,
+        mipmap: int = 0,
+    ) -> oiio.ImageBuf:
+        """
 
-    def read_array(self, subimage: int, mipmap: int) -> numpy.ndarray:
-        return self.get_image_buf(subimage, mipmap).get_pixels(oiio.TypeFloat)
+        Args:
+            channels: list of name/index of channels to retrieve. ex: ("R", "A")
+            subimage: index of the subimage to retrieve. Usually 0.
+            mipmap: index of the mipmap to retrieve. Usually 0.
+
+        Returns:
+            ImageBUf instance as specified
+        """
+        buf = oiio.ImageBuf(str(self.path), subimage, mipmap)
+        if channels:
+            buf = oiio.ImageBufAlgo.channels(buf, channels)
+        return buf
+
+    def read_array(
+        self,
+        channels: Optional[tuple] = None,
+        subimage: int = 0,
+        mipmap: int = 0,
+    ) -> numpy.ndarray:
+        """
+
+        Args:
+            channels: list of name/index of channels to retrieve. ex: ("R", "A")
+            subimage: index of the subimage to retrieve. Usually 0.
+            mipmap: index of the mipmap to retrieve. Usually 0.
+
+        Returns:
+            pixel data as floating point numpy array
+        """
+        return self.get_image_buf(channels, subimage, mipmap).get_pixels(oiio.TypeFloat)
 
 
 @dataclass
