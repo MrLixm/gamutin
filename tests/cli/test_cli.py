1388e57d0c5b987ef2973a1609afc060116a9c46
Liam Collod <lcollod@gmail.com>
2022-11-27 14:46:28 +0100

    [tests] added first tests for cli

---
+++ tests/cli/test_cli.py
@@ -0,0 +1,59 @@
+from click.testing import CliRunner
+
+from pgcheck.cli.interface import cli
+
+
+def test_cli_basic(imagepath_wheel_mchannel):
+
+    runner = CliRunner()
+    # just make sure they don't raise errors
+    result = runner.invoke(cli, ["--debug", "colorspaces"])
+    assert result.exit_code == 0
+
+    result = runner.invoke(cli, ["--debug", "blendmodes"])
+    assert result.exit_code == 0
+
+    result = runner.invoke(cli, ["--debug", "imageinfo", str(imagepath_wheel_mchannel)])
+    assert result.exit_code == 0
+
+
+def test_cli_check(imagepath_wheel_mchannel, imagepath_color_bars_alpha, tmp_path):
+
+    runner = CliRunner()
+
+    tmp_target_path = tmp_path / (imagepath_color_bars_alpha.stem + ".jpg")
+
+    result = runner.invoke(
+        cli,
+        [
+            "--debug",
+            "check",
+            str(imagepath_color_bars_alpha),
+            str(tmp_target_path),
+            "--colorspace",
+            "sRGB",
+            "--target_colorspace",
+            "sRGB",
+        ],
+    )
+    assert result.exit_code == 0
+
+    tmp_target_path = tmp_path / (imagepath_wheel_mchannel.stem + ".jpg")
+
+    result = runner.invoke(
+        cli,
+        [
+            "--debug",
+            "check",
+            str(imagepath_wheel_mchannel),
+            str(tmp_target_path),
+            "--colorspace",
+            "sRGB",
+            "--target_colorspace",
+            "sRGB",
+            "--blend_mode",
+            "invalid_replace",
+            "--use_alpha_as_mask",
+        ],
+    )
+    assert result.exit_code == 0
