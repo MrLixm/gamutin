eef2d298e4cdcee5eb92921ce9d50bc46289e30f
Liam Collod <lcollod@gmail.com>
2022-11-28 19:17:49 +0100

    [tests.cli] added test_cli_basic_raise

--- tests/cli/test_cli.py
+++ tests/cli/test_cli.py
@@ -1,5 +1,6 @@
 from click.testing import CliRunner
 
+import pgcheck.core.colorspaces
 from pgcheck.cli.interface import cli
 
 
@@ -15,6 +16,85 @@ def test_cli_basic(imagepath_wheel_mchannel):
 
     result = runner.invoke(cli, ["--debug", "imageinfo", str(imagepath_wheel_mchannel)])
     assert result.exit_code == 0
+
+
+def test_cli_basic_raise(imagepath_wheel_mchannel, tmp_path):
+
+    runner = CliRunner()
+
+    tmp_target_path = tmp_path / (imagepath_wheel_mchannel.stem + ".jpg")
+
+    error_colorspace = "sRGBBB"
+
+    result = runner.invoke(
+        cli,
+        [
+            "--debug",
+            "check",
+            str(imagepath_wheel_mchannel),
+            str(tmp_target_path),
+            "--colorspace",
+            error_colorspace,
+            "--target_colorspace",
+            "aces",
+        ],
+    )
+    assert result.exit_code != 0
+    assert error_colorspace in str(result.exception)
+
+    error_colorspace = "sRGBBB"
+
+    result = runner.invoke(
+        cli,
+        [
+            "--debug",
+            "check",
+            str(imagepath_wheel_mchannel),
+            str(tmp_target_path),
+            "--colorspace",
+            "aces",
+            "--target_colorspace",
+            error_colorspace,
+        ],
+    )
+    assert result.exit_code != 0
+    assert error_colorspace in str(result.exception)
+
+    error_colorspace = pgcheck.core.colorspaces.POINTER_GAMUT_COLORSPACE.name
+
+    result = runner.invoke(
+        cli,
+        [
+            "--debug",
+            "check",
+            str(imagepath_wheel_mchannel),
+            str(tmp_target_path),
+            "--colorspace",
+            error_colorspace,
+            "--target_colorspace",
+            "sRGB",
+        ],
+    )
+    assert result.exit_code != 0
+    assert error_colorspace in str(result.exception)
+
+    error_colorspace = pgcheck.core.colorspaces.POINTER_GAMUT_COLORSPACE.name
+
+    result = runner.invoke(
+        cli,
+        [
+            "--debug",
+            "check",
+            str(imagepath_wheel_mchannel),
+            str(tmp_target_path),
+            "--colorspace",
+            "sRGB",
+            "--target_colorspace",
+            error_colorspace,
+        ],
+    )
+    assert result.exit_code != 0
+    assert error_colorspace in str(result.exception)
 
 
 def test_cli_check(imagepath_wheel_mchannel, imagepath_color_bars_alpha, tmp_path):
