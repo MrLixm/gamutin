06cfa1b70b3c73cf664965418c28d39e119d9ed4
Liam Collod <lcollod@gmail.com>
2022-12-04 18:37:20 +0100

    [editor.resources] added icons for path selector

--- pgcheck/editor/resources/__init__.py
+++ pgcheck/editor/resources/__init__.py
@@ -8,5 +8,13 @@ class ResourceLibrary:
 
         self.icon_main = self.root_icon / "icon.ico"
 
+        self.icon_file_check_outline = self.root_icon / "file-check-outline.svg"
+        self.icon_file_multiple_outline = self.root_icon / "file-multiple-outline.svg"
+        self.icon_file_outline = self.root_icon / "file-outline.svg"
+        self.icon_folder = self.root_icon / "folder.svg"
+        self.icon_folder_check = self.root_icon / "folder-check.svg"
+        self.icon_information = self.root_icon / "information.svg"
+        self.icon_alert_outline = self.root_icon / "alert-circle-outline.svg"
+
     def register(self):
         pass
