f64ccfead1b5df4e46621859a181759ee967bc14
Liam Collod <lcollod@gmail.com>
2022-12-07 22:06:30 +0100

    [resources.styles] added variables to test.qss

--- pgcheck/editor/resources/__init__.py
+++ pgcheck/editor/resources/__init__.py
@@ -18,7 +18,7 @@ class DefaultStyleTheme(StyleTheme):
 
     color_text_base = ColorQtProperty((250, 250, 250, 255))
     color_error_red = ColorQtProperty((187, 76, 76, 255))
-    size_border_radius_base = LengthQtProperty(5)
+    size_border_radius_base = LengthQtProperty(4)
 
 
 class ResourceLibrary:
