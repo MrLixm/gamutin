f4cf29c32293ae7ceb78024dceaba1038de68bb9
Liam Collod <lcollod@gmail.com>
2022-12-11 16:30:15 +0100

    [core.colorspaces] added `categories` module
    to help at sorting colorspaces. that's a wip

---
+++ gamutin/core/colorspaces/categories.py
@@ -0,0 +1,13 @@
+import enum
+
+
+class ColorspaceCategory(enum.Enum):
+    """
+    A colorspace can belong to multiple of those category. And a categoryc oudl be a child
+    of another category, but this is defined at the editor level.
+    """
+
+    aces = "ACES"
+    camera = "Camera"
+    p3 = "P3 Family"
+    common = "Common"
