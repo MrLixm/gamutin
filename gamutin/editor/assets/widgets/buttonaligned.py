c2cebc27c2b249efaa06fc36ca6daf688f869c3d
Liam Collod <lcollod@gmail.com>
2022-12-18 23:56:59 +0100

    [editor...widgets] PushButtonAligned: fixed main logic
    nearly working perfectly

--- gamutin/editor/assets/widgets/buttonaligned.py
+++ gamutin/editor/assets/widgets/buttonaligned.py
@@ -1,6 +1,7 @@
 __all__ = ("PushButtonAligned",)
 
 import logging
+from typing import Optional
 
 from Qt import QtWidgets
 from Qt import QtCore
@@ -23,7 +24,7 @@ class PushButtonAligned(QtWidgets.QPushButton):
         | [icon] text          |
         ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
 
-        pin_icon_left, align_text_left
+        pin_icon_left, align_text_right
         ————————————————————————
         | [icon]          text |
         ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
@@ -44,26 +45,20 @@ class PushButtonAligned(QtWidgets.QPushButton):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        self.is_icon_pinned: bool = False
+        self.text_h_alignment: QtCore.Qt.AlignmentFlag = QtCore.Qt.AlignLeft
+        self.icon_h_alignment: Optional[QtCore.Qt.AlignmentFlag] = None
+
         # 1. Create
         self.layout = QtWidgets.QHBoxLayout(self)
         self.label_icon = QtWidgets.QLabel()
         self.label_text = QtWidgets.QLabel(self.text())
-        self.stretch_l = QtWidgets.QSpacerItem(
-            16,
-            16,
-            QtWidgets.QSizePolicy.Expanding,
-            QtWidgets.QSizePolicy.Expanding,
-        )
-        self.stretch_r = QtWidgets.QSpacerItem(
-            16,
-            16,
-            QtWidgets.QSizePolicy.Expanding,
-            QtWidgets.QSizePolicy.Expanding,
-        )
 
         # 2. Add
-        self.layout.addWidget(self.label_text)
+        self.layout.addStretch(0)
         self.layout.addWidget(self.label_icon)
+        self.layout.addWidget(self.label_text)
+        self.layout.addStretch(0)
 
         # 3. Modify
         self.layout.setContentsMargins(0, 0, 0, 0)
@@ -71,6 +66,8 @@ class PushButtonAligned(QtWidgets.QPushButton):
             label.setAttribute(QtCore.Qt.WA_TranslucentBackground)
             label.setAttribute(QtCore.Qt.WA_TransparentForMouseEvents)
 
+        self.label_text.setObjectName(f"{self.__class__.__name__}:text")
+        self.label_icon.setObjectName(f"{self.__class__.__name__}:icon")
         self.label_icon.setSizePolicy(
             QtWidgets.QSizePolicy.Fixed,
             QtWidgets.QSizePolicy.Fixed,
@@ -79,67 +76,167 @@ class PushButtonAligned(QtWidgets.QPushButton):
         self.label_icon.setPixmap(self.icon().pixmap(self.iconSize()))
         self.setIcon(QtGui.QIcon())
 
-        # set default look
+        self.set_text_alignment(QtCore.Qt.AlignCenter)
+
+    def setContentsMargins(
+        self,
+        left: Optional[int],
+        top: Optional[int],
+        right: Optional[int],
+        bottom: Optional[int],
+    ):
+        """
+
+        Args:
+            left: use existing margins if None, else override it
+            top: use existing margins if None, else override it
+            right: use existing margins if None, else override it
+            bottom: use existing margins if None, else override it
+        """
+        margins = self.getContentsMargins()
+        left = left if left is not None else margins[0]
+        top = top if top is not None else margins[1]
+        right = right if right is not None else margins[2]
+        bottom = bottom if bottom is not None else margins[3]
+        super().setContentsMargins(left, top, right, bottom)
+
+    def _update_layout(
+        self,
+        text_v_alignment: QtCore.Qt.AlignmentFlag = None,
+    ):
+        """
+        Call an update on the icon relative to the current text position.
+        """
         self.align_icon_left()
-        self.align_text_center()
+        self.layout.removeWidget(self.label_icon)
+        self.layout.removeWidget(self.label_text)
+        text_v_alignment = text_v_alignment or QtCore.Qt.AlignVCenter
+
+        if self.text_h_alignment == QtCore.Qt.AlignLeft:
+            # [i][t][s][s]
+            self.layout.insertWidget(0, self.label_text)
+            self.layout.insertWidget(0, self.label_icon)
+
+        elif self.text_h_alignment == QtCore.Qt.AlignRight:
+            # [s][s][i][t]
+            self.layout.insertWidget(-1, self.label_icon)
+            self.layout.insertWidget(-1, self.label_text)
+
+        elif self.text_h_alignment == QtCore.Qt.AlignCenter:
+            # [s][i][t][s]
+            self.layout.insertWidget(1, self.label_text)
+            self.layout.insertWidget(1, self.label_icon)
+
+        else:
+            raise ValueError(f"Unsupported value for {int(self.text_h_alignment)=}")
+
+        logger.debug(
+            f"[{self.__class__.__name__}][_update_layout](text)  {self._repr_layout()}"
+        )
+
+        self.label_text.setAlignment(self.text_h_alignment | text_v_alignment)
+
+        if self.icon_h_alignment == QtCore.Qt.AlignLeft:
+
+            if self.is_icon_pinned:
+                self.layout.insertWidget(0, self.label_icon)
+            else:
+                self.align_icon_left()
+
+        elif self.icon_h_alignment == QtCore.Qt.AlignRight:
+
+            if self.is_icon_pinned:
+                self.layout.insertWidget(-1, self.label_icon)
+            else:
+                self.align_icon_right()
+
+        elif self.icon_h_alignment is None:
+            pass
+
+        else:
+            raise ValueError(f"Unsupported value for {self.icon_h_alignment=}")
+
+        logger.debug(
+            f"[{self.__class__.__name__}][_update_layout](icon)    {self._repr_layout()}"
+        )
+        return
+
+    def _repr_layout(self) -> str:
+        """
+        Return the layout structure as a string like ``[<>][i][t][<>]``.
+        """
+
+        layout_repr = ""
+        for child_index in range(self.layout.count()):
+            l_item = self.layout.itemAt(child_index)
+            if l_item.spacerItem():
+                layout_repr += "[<>]"
+            else:
+                layout_repr += f"[{l_item.widget().objectName().split(':')[-1][0]}]"
+
+        if self.layout.direction() == self.layout.RightToLeft:
+            layout_repr = layout_repr[::-1].replace("[", "$").replace("]", "@")
+            layout_repr = layout_repr.replace("$", "]").replace("@", "[")
+            layout_repr += "(reverted)"
+
+        return layout_repr
 
     def align_icon_left(self):
         """
         Align icon relative to the text.
         """
-        self.layout.setDirection(self.layout.RightToLeft)
-        self.layout.removeItem(self.stretch_l)
-        self.layout.removeItem(self.stretch_r)
-        self.layout.insertItem(0, self.stretch_l)
-        self.layout.insertItem(-1, self.stretch_r)
+        self.layout.setDirection(self.layout.LeftToRight)
 
     def align_icon_right(self):
         """
         Align icon relative to the text.
         """
-        self.layout.setDirection(self.layout.LeftToRight)
-        self.layout.removeItem(self.stretch_l)
-        self.layout.removeItem(self.stretch_r)
-        self.layout.insertItem(0, self.stretch_l)
-        self.layout.insertItem(-1, self.stretch_r)
+        self.layout.setDirection(self.layout.RightToLeft)
 
-    def align_text_left(self, margin=0):
-        self.label_text.setAlignment(QtCore.Qt.AlignLeft | QtCore.Qt.AlignVCenter)
-        left, top, right, bottom = self.getContentsMargins()
-        self.setContentsMargins(margin, top, right, bottom)
+    def align_text_left(self, margin=None):
 
-    def align_text_right(self, margin=0):
-        self.label_text.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignVCenter)
-        left, top, right, bottom = self.getContentsMargins()
-        self.setContentsMargins(left, top, margin, bottom)
+        self.text_h_alignment = QtCore.Qt.AlignLeft
+        self._update_layout(QtCore.Qt.AlignVCenter)
+
+        self.setContentsMargins(margin, None, None, None)
+
+    def align_text_right(self, margin=None):
+
+        self.text_h_alignment = QtCore.Qt.AlignRight
+        self._update_layout(QtCore.Qt.AlignVCenter)
+
+        self.setContentsMargins(None, None, margin, None)
 
     def align_text_center(self):
-        self.label_text.setAlignment(QtCore.Qt.AlignCenter)
 
-    def pin_icon_left(self, margin=0):
+        self.text_h_alignment = QtCore.Qt.AlignCenter
+        self._update_layout(QtCore.Qt.AlignVCenter)
+
+    def pin_icon_left(self, margin=None):
         """
         Align icon relative to the PushButton borders.
         """
-        self.align_icon_left()
-        self.layout.removeItem(self.stretch_l)
-        self.layout.removeItem(self.stretch_r)
+        self.is_icon_pinned = True
+        self.icon_h_alignment = QtCore.Qt.AlignLeft
+        self._update_layout()
+        self.setContentsMargins(margin, None, None, None)
 
-        left, top, right, bottom = self.getContentsMargins()
-        self.setContentsMargins(margin, top, right, bottom)
-
-    def pin_icon_right(self, margin=0):
+    def pin_icon_right(self, margin=None):
         """
         Align icon relative to the PushButton borders.
         """
-        self.align_icon_right()
-        self.layout.removeItem(self.stretch_l)
-        self.layout.removeItem(self.stretch_r)
+        self.is_icon_pinned = True
+        self.icon_h_alignment = QtCore.Qt.AlignRight
+        self._update_layout()
+        self.setContentsMargins(None, None, margin, None)
 
-        left, top, right, bottom = self.getContentsMargins()
-        self.setContentsMargins(left, top, margin, bottom)
-
-    def set_text_alignment(self, alignment: QtCore.Qt.Alignment):
-        self.label_text.setAlignment(alignment)
+    def set_text_alignment(
+        self,
+        alignment_h: QtCore.Qt.AlignmentFlag,
+        alignment_v: Optional[QtCore.Qt.AlignmentFlag] = None,
+    ):
+        self.text_h_alignment = alignment_h
+        self._update_layout(alignment_v)
 
 
 def _test_interface():
