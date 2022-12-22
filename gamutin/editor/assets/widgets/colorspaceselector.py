b5d7070bd49a69194d4505fd7a5edf4cc0c03ab7
Liam Collod <lcollod@gmail.com>
2022-12-22 21:00:38 +0100

    [editor...widgets] ColorspaceSelector: using PushButtonAligned, proper menu

--- gamutin/editor/assets/widgets/colorspaceselector.py
+++ gamutin/editor/assets/widgets/colorspaceselector.py
@@ -1,11 +1,13 @@
 __all__ = ("ColorspaceSelector",)
 
 import logging
+from functools import partial
 
 from Qt import QtCore
 from Qt import QtWidgets
 
 import gamutin.core.colorspaces
+from gamutin.editor.assets.widgets import PushButtonAligned
 
 logger = logging.getLogger(__name__)
 
@@ -20,35 +22,65 @@ class ColorspaceSelector(QtWidgets.QWidget):
     def __init__(self, parent=None):
         # type: (QtWidgets.QWidget) -> None
         super().__init__(parent)
+
+        self._current_colorspace: gamutin.core.colorspaces.RgbColorspace = (
+            gamutin.core.colorspaces.get_colorspace("sRGB")
+        )
+
         self.cookUI()
         self.bakeUI()
+
+        self.set_current_colorspace(self._current_colorspace)
 
     def cookUI(self):
         # 1. Create
         self.layout = QtWidgets.QHBoxLayout()
-        self.combobox_colorspace = QtWidgets.QComboBox()
+        self.button_colorspace = PushButtonAligned()
+        self.menu_colorspace = QtWidgets.QMenu()
         self.checkbox_force_linear_target = QtWidgets.QCheckBox(
             "Force Linear Transfer-Function"
         )
 
         # 2. Add
         self.setLayout(self.layout)
-        self.layout.addWidget(self.combobox_colorspace)
+        self.layout.addWidget(self.button_colorspace)
         self.layout.addWidget(self.checkbox_force_linear_target)
 
         # 3. Modify
         self.layout.setContentsMargins(*(0,) * 4)
+        self.button_colorspace.setMenu(self.menu_colorspace)
+        self.button_colorspace.align_text_left()
         # 4. Connections
         return
 
     def bakeUI(self):
 
-        self.combobox_colorspace.clear()
-
+        self.menu_colorspace.clear()
+        menu_categories = {}
         for colorspace in gamutin.core.colorspaces.get_available_colorspaces():
 
-            self.combobox_colorspace.addItem(colorspace.name, colorspace)
-            self.combobox_colorspace.model().sort(0, QtCore.Qt.AscendingOrder)
+            colorspace_categories: list[str] = [
+                str(colorspace_category.value)
+                for colorspace_category in colorspace.categories
+            ]
+            colorspace_categories.append("All")
+
+            for category in colorspace_categories:
+
+                menu = menu_categories.get(category)
+                if not menu:
+                    menu = QtWidgets.QMenu(category)
+                    menu_categories[category] = menu
+
+                action = QtWidgets.QAction(colorspace.name, self)
+                action.setToolTip(colorspace.description)
+                action.triggered.connect(
+                    partial(self.set_current_colorspace, colorspace)
+                )
+                menu.addAction(action)
+
+        for menu_category in menu_categories.values():
+            self.menu_colorspace.addMenu(menu_category)
 
     def set_force_linear_visible(self, visible: bool):
         """
@@ -64,12 +96,23 @@ class ColorspaceSelector(QtWidgets.QWidget):
         """
         self.checkbox_force_linear_target.setChecked(enable)
 
+    def set_current_colorspace(
+        self,
+        colorspace: gamutin.core.colorspaces.RgbColorspace,
+    ):
+        """
+        Set the given colorspace to be the currently actively selected one in the interface.
+        """
+        self.button_colorspace.setText(colorspace.name)
+        self.button_colorspace.setToolTip(colorspace.description)
+        self._current_colorspace = colorspace
+
     def get_current_colorspace(self) -> gamutin.core.colorspaces.RgbColorspace:
         """
         Return the colorspace currently selected in the interface.
         As a linear variant if asked as such.
         """
-        colorspace_name = self.combobox_colorspace.currentText()
+        colorspace_name = self._current_colorspace.name
         force_linear = self.checkbox_force_linear_target.isChecked()
         if force_linear:
             colorspace_name = f"{colorspace_name}:linear"
