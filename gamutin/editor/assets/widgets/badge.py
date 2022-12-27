23773c58b58d893ae108abf75a5fd622e1098c9f
Liam Collod <lcollod@gmail.com>
2022-12-27 19:20:11 +0100

    [editor...widgets.badge] wip2 classes structure

--- gamutin/editor/assets/widgets/badge.py
+++ gamutin/editor/assets/widgets/badge.py
@@ -1,9 +1,8 @@
-__all__ = ("",)
+from __future__ import annotations
 
-import abc
+__all__ = ("BadgeOverlayWidget",)
+
 import logging
-from functools import partial
-from typing import Optional
 
 from Qt import QtWidgets
 from Qt import QtGui
@@ -12,34 +11,36 @@ from Qt import QtCore
 logger = logging.getLogger(__name__)
 
 
+def paint_badge(widget: BadgeMixin, event: QtGui.QPaintEvent):
+    qpainter = QtGui.QPainter()
+    qpainter.begin(widget)
+
+    rect = event.rect()
+    target_rect = QtCore.QRect(
+        rect.right() - widget.badge_size - 1,
+        rect.top() + widget.badge_size,
+        widget.badge_size,
+        widget.badge_size,
+    )
+
+    qpainter.setRenderHints(qpainter.Antialiasing)
+    qpainter.setPen(QtGui.QPen(QtCore.Qt.transparent, 1))
+    qpainter.setBrush(QtGui.QColor(250, 40, 40))
+    qpainter.drawEllipse(target_rect.center(), widget.badge_size, widget.badge_size)
+
+    if widget.badge_number > 0:
+        qpainter.setPen(QtGui.QColor(250, 250, 250))
+        qpainter.setFont(QtGui.QFont("Sans", 6))
+        qpainter.drawText(target_rect, QtCore.Qt.AlignCenter, str(widget.badge_number))
+
+    qpainter.end()
+
+
 class BadgeMetaclass(type):
     def __init__(mcs, clsname, bases, attrs):
         super().__init__(clsname, bases, attrs)
-        mcs.number = 0
-        mcs.paint_badge = BadgeMetaclass.paint_badge
-
-    @classmethod
-    def paint_badge(mcs, self, event: QtGui.QPaintEvent, *args, **kwargs):
-
-        qpainter = QtGui.QPainter()
-        qpainter.begin(self)
-
-        rect = event.rect()
-        target_rect = QtCore.QRect(
-            rect.left(),
-            rect.top(),
-            rect.width(),
-            rect.height(),
-        )
-
-        qpainter.setRenderHints(qpainter.Antialiasing)
-        qpainter.setPen(QtGui.QPen(QtCore.Qt.white, 1))
-        qpainter.setBrush(QtCore.Qt.red)
-        qpainter.drawEllipse(target_rect.center(), 15, 15)
-        if self.number > 0:
-            qpainter.drawText(target_rect, QtCore.Qt.AlignCenter, str(self.number))
-
-        qpainter.end()
+        mcs.badge_number = 0
+        mcs.badge_size = 6
 
 
 class BadgeMixin(type(QtCore.QObject), BadgeMetaclass):
@@ -47,7 +48,8 @@ class BadgeMixin(type(QtCore.QObject), BadgeMetaclass):
 
 
 class BadgeOverlayWidget(object, metaclass=BadgeMixin):
-    pass
+    def paint_badge(self, event: QtGui.QPaintEvent):
+        paint_badge(self, event)
 
 
 def _test_interface():
@@ -57,15 +59,19 @@ def _test_interface():
     from gamutin.editor.main import getQApp
     from gamutin.editor.testing import get_testing_window
 
-    class TestLabel(QtWidgets.QLabel, metaclass=BadgeMixin):
+    class TestLabel(QtWidgets.QLabel, BadgeOverlayWidget):
         def __init__(self):
             super().__init__()
             self.setText("this is some test text")
-            print(self.number)
+            self.setSizePolicy(
+                QtWidgets.QSizePolicy.Fixed,
+                QtWidgets.QSizePolicy.Fixed,
+            )
+            assert self.badge_number is not None
 
         def paintEvent(self, event: QtGui.QPaintEvent):
             super().paintEvent(event)
-            self.paint_badge(self, event)
+            self.paint_badge(event)
 
     _configureLogging()
     app = getQApp()
@@ -75,10 +81,27 @@ def _test_interface():
     layout = QtWidgets.QGridLayout()
 
     widget = TestLabel()
-    widget.number = 3
     layout.addWidget(widget)
 
     widget = TestLabel()
+    widget.badge_number = 3
+    layout.addWidget(widget)
+
+    widget = TestLabel()
+    widget.badge_number = 3
+    widget.setMargin(20)
+    layout.addWidget(widget)
+
+    widget = TestLabel()
+    widget.badge_number = 3
+    widget.setMargin(20)
+    widget.badge_size = 8
+    layout.addWidget(widget)
+
+    widget = TestLabel()
+    widget.badge_number = 3
+    widget.setMargin(20)
+    widget.badge_size = 16
     layout.addWidget(widget)
 
     window.add_layout(layout)
