5feb03d2400989cfb37580e4b6f086e986b4966f
Liam Collod <lcollod@gmail.com>
2022-12-27 21:01:33 +0100

    [editor...widgets.badge] finishing

--- gamutin/editor/assets/widgets/badge.py
+++ gamutin/editor/assets/widgets/badge.py
@@ -3,35 +3,51 @@ from __future__ import annotations
 __all__ = ("BadgeOverlayWidget",)
 
 import logging
+from typing import Union
 
 from Qt import QtWidgets
 from Qt import QtGui
 from Qt import QtCore
 
+from gamutin.editor.cfg import resources
+
 logger = logging.getLogger(__name__)
 
 
-def paint_badge(widget: BadgeMixin, event: QtGui.QPaintEvent):
+def paint_badge(widget: Union[BadgeMixin, QtWidgets.QWidget], event: QtGui.QPaintEvent):
+
     qpainter = QtGui.QPainter()
     qpainter.begin(widget)
+    color = widget.badge_color
+    if not widget.isEnabled():
+        color = QtGui.QColor.fromHsl(color.hue(), 0, color.lightness(), color.alpha())
 
-    rect = event.rect()
-    target_rect = QtCore.QRect(
-        rect.right() - widget.badge_size - 1,
-        rect.top() + widget.badge_size,
+    source_rect = event.rect()
+    target_rect = QtCore.QRectF(
+        source_rect.width() - widget.badge_size,
+        source_rect.top(),
         widget.badge_size,
         widget.badge_size,
     )
 
     qpainter.setRenderHints(qpainter.Antialiasing)
     qpainter.setPen(QtGui.QPen(QtCore.Qt.transparent, 1))
-    qpainter.setBrush(QtGui.QColor(250, 40, 40))
-    qpainter.drawEllipse(target_rect.center(), widget.badge_size, widget.badge_size)
+    qpainter.setBrush(color)
+    # qpainter.drawRect(target_rect)
+    qpainter.drawEllipse(
+        target_rect.center(),
+        widget.badge_size / 2,
+        widget.badge_size / 2,
+    )
 
     if widget.badge_number > 0:
-        qpainter.setPen(QtGui.QColor(250, 250, 250))
-        qpainter.setFont(QtGui.QFont("Sans", 6))
-        qpainter.drawText(target_rect, QtCore.Qt.AlignCenter, str(widget.badge_number))
+        qpainter.setPen(widget.badge_color_text)
+        qpainter.setFont(QtGui.QFont("Mono", int((widget.badge_size / 1.3) ** 0.85)))
+        qpainter.drawText(
+            target_rect,
+            QtCore.Qt.AlignCenter,
+            str(widget.badge_number),
+        )
 
     qpainter.end()
 
@@ -39,8 +55,14 @@ def paint_badge(widget: BadgeMixin, event: QtGui.QPaintEvent):
 class BadgeMetaclass(type):
     def __init__(mcs, clsname, bases, attrs):
         super().__init__(clsname, bases, attrs)
-        mcs.badge_number = 0
-        mcs.badge_size = 6
+        mcs.badge_number: int = 0
+        mcs.badge_size: int = 12
+        mcs.badge_color: QtGui.QColor = (
+            resources.theme_default.color_notification.value.as_qcolor()
+        )
+        mcs.badge_color_text: QtGui.QColor = (
+            resources.theme_default.color_text_base.value.as_qcolor()
+        )
 
 
 class BadgeMixin(type(QtCore.QObject), BadgeMetaclass):
@@ -48,6 +70,17 @@ class BadgeMixin(type(QtCore.QObject), BadgeMetaclass):
 
 
 class BadgeOverlayWidget(object, metaclass=BadgeMixin):
+    def __init__(self):
+        # for type hints only
+        self.badge_number: int = 0
+        self.badge_size: int = 12
+        self.badge_color: QtGui.QColor = (
+            resources.theme_default.color_notification.value.as_qcolor()
+        )
+        self.badge_color_text: QtGui.QColor = (
+            resources.theme_default.color_text_base.value.as_qcolor()
+        )
+
     def paint_badge(self, event: QtGui.QPaintEvent):
         paint_badge(self, event)
 
@@ -67,6 +100,16 @@ def _test_interface():
                 QtWidgets.QSizePolicy.Fixed,
                 QtWidgets.QSizePolicy.Fixed,
             )
+            assert self.badge_number is not None
+
+        def paintEvent(self, event: QtGui.QPaintEvent):
+            super().paintEvent(event)
+            self.paint_badge(event)
+
+    class TestButton(QtWidgets.QPushButton, BadgeOverlayWidget):
+        def __init__(self):
+            super().__init__()
+            self.setText("this is some test text")
             assert self.badge_number is not None
 
         def paintEvent(self, event: QtGui.QPaintEvent):
@@ -101,7 +144,37 @@ def _test_interface():
     widget = TestLabel()
     widget.badge_number = 3
     widget.setMargin(20)
-    widget.badge_size = 16
+    widget.badge_size = 12
+    layout.addWidget(widget)
+
+    widget = TestLabel()
+    widget.badge_number = 3
+    widget.setMargin(20)
+    widget.badge_size = 24
+    layout.addWidget(widget)
+
+    widget = TestLabel()
+    widget.badge_number = 3
+    widget.setMargin(20)
+    widget.badge_size = 30
+    layout.addWidget(widget)
+
+    widget = TestLabel()
+    widget.badge_number = 16
+    widget.setMargin(20)
+    widget.setStyleSheet(
+        f"font-family: {QtGui.QFontDatabase().families()[2]}; font-size: 15px;"
+    )
+    widget.badge_size = 20
+    layout.addWidget(widget)
+
+    widget = TestButton()
+    widget.badge_number = 3
+    layout.addWidget(widget)
+
+    widget = TestButton()
+    widget.badge_number = 3
+    widget.setStyleSheet("margin: 5px;")
     layout.addWidget(widget)
 
     window.add_layout(layout)
