dc09f84cf04bee2982f1ae8a65bb7fc6d8fa2ae3
Liam Collod <lcollod@gmail.com>
2023-02-26 20:23:18 +0100

    [editor...dialogs.issue] improved new issue url , now pref-filled

--- gamutin/editor/assets/dialogs/issue.py
+++ gamutin/editor/assets/dialogs/issue.py
@@ -1,6 +1,6 @@
 import math
+import urllib.parse
 import webbrowser
-from functools import partial
 from typing import Optional
 
 from Qt import QtWidgets
@@ -33,25 +33,42 @@ class IssueDialog(QtWidgets.QDialog):
             f"to create a new issue on the {gamutin.c.name} repository.\n\n"
             "When submitting an issue please copy and paste the following information:"
         )
-        self.ctx_wgt = ContextWidget(self)
+        self.context_widget = ContextWidget(self)
         self.btn_report = QtWidgets.QPushButton("Report a New Issue")
 
         # 2. Add
         self.setLayout(self.lyt)
         self.lyt.addWidget(self.lbl_title)
         self.lyt.addWidget(self.lbl_body)
-        self.lyt.addWidget(self.ctx_wgt)
+        self.lyt.addWidget(self.context_widget)
         self.lyt.addWidget(self.btn_report)
 
         # 3. Modify
         self.lbl_title.setAlignment(QtCore.Qt.AlignCenter)
         self.lbl_body.setWordWrap(True)
         # 4. Connections
-        self.btn_report.clicked.connect(
-            partial(webbrowser.open, f"{gamutin.c.vcs_url}/issues")
-        )
+        self.btn_report.clicked.connect(self.open_new_issue_url)
 
         return
 
     def bakeUI(self):
-        self.ctx_wgt.bakeUI()
+        self.context_widget.bakeUI()
+
+    def open_new_issue_url(self):
+        """
+        Open the url to create a new issue on the repo in the user's default webbrowser
+        """
+        url = f"{gamutin.c.vcs_url}/issues/new"
+        context = self.context_widget.getContext()
+
+        issue_description = (
+            f"# description\n"
+            f"\nwrite a clear description of your issue here\n"
+            f"\n# context\n\n```\n{context}\n```\n"
+        )
+        issue_description_safe = urllib.parse.quote_plus(issue_description)
+
+        url = f"{url}?body={issue_description_safe}"
+
+        webbrowser.open(url)
+        return
