3c7b7a31f9c9056c13845e0a8e89aacb2bc7e0e4
Liam Collod <lcollod@gmail.com>
2023-03-25 17:09:23 +0100

    [editor.utils] added `color_interpolate` function

--- gamutin/editor/utils.py
+++ gamutin/editor/utils.py
@@ -1,5 +1,6 @@
 __all__ = (
     "copy_to_clipboard",
+    "color_interpolate",
     "block_signals",
 )
 
@@ -8,6 +9,7 @@ from contextlib import contextmanager
 
 from Qt import QtWidgets
 from Qt import QtCore
+from Qt import QtGui
 
 logger = logging.getLogger(__name__)
 
@@ -40,3 +42,23 @@ def block_signals(qtobject: QtCore.QObject):
         yield
     finally:
         qtobject.blockSignals(False)
+
+
+def color_interpolate(color_start: QtGui.QColor, color_end: QtGui.QColor, ratio: float):
+    """
+    Return the interpolated color between start and end for the given ratio.
+
+    All of this because QGradient does not have a ``colorAt(pos)`` method ...
+
+    Args:
+        color_start: any color
+        color_end: any color
+        ratio: strict 0-1 range
+
+    Returns:
+        QColor instance
+    """
+    r = (1 - ratio) * color_start.redF() + ratio * color_end.redF()
+    g = (1 - ratio) * color_start.greenF() + ratio * color_end.greenF()
+    b = (1 - ratio) * color_start.blueF() + ratio * color_end.blueF()
+    return QtGui.QColor.fromRgbF(r, g, b)
