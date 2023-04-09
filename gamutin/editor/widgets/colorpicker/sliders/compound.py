cc8ec5ed7401d786fdf66a698c38a2b400ea4ee1
Liam Collod <lcollod@gmail.com>
2023-04-09 17:35:12 +0200

    [editor...colorpicker.slider.compound] improved widget value update

--- gamutin/editor/widgets/colorpicker/sliders/compound.py
+++ gamutin/editor/widgets/colorpicker/sliders/compound.py
@@ -16,6 +16,12 @@ logger = logging.getLogger(__name__)
 
 
 class FloatSliderWidget(QtWidgets.QWidget):
+    """
+    Widget to select a single float value.
+
+    Made of a combo box displaying the current value and a horizontal slider.
+    """
+
     value_changed_signal = QtCore.Signal()
 
     def __init__(self, parent: QtWidgets.QWidget = None):
@@ -23,15 +29,13 @@ class FloatSliderWidget(QtWidgets.QWidget):
 
         self._minimum: float = 0.0
         self._maximum: float = 1.0
-        self._value: float = 1.0
+        self._value: float = 0.0
         self._out_of_range_allowed: bool = False
-        self._link_field_slider_range: bool = False
 
         # 1. Create
         self.layout = QtWidgets.QHBoxLayout()
         self.slider = FloatGradientSlider()
         self.field_value = QtWidgets.QDoubleSpinBox()
-        self.button_link = QtWidgets.QPushButton("<>")
 
         # 2. Add
         self.setLayout(self.layout)
@@ -44,11 +48,14 @@ class FloatSliderWidget(QtWidgets.QWidget):
             QtWidgets.QSizePolicy.Fixed,
             QtWidgets.QSizePolicy.Fixed,
         )
+        self.field_value.setSingleStep(0.01)
 
         # 4. Connections
         self.slider.value_changed_signal.connect(self.on_value_slider_changed)
         self.field_value.valueChanged.connect(self.on_value_field_changed)
 
+        self.updated_field()
+        self.update_slider()
         return
 
     @property
@@ -60,6 +67,8 @@ class FloatSliderWidget(QtWidgets.QWidget):
         self._minimum = minimum_value
         if not self._out_of_range_allowed:
             self.field_value.setMinimum(minimum_value)
+        self.updated_field()
+        self.update_slider()
 
     @property
     def maximum(self) -> float:
@@ -70,6 +79,8 @@ class FloatSliderWidget(QtWidgets.QWidget):
         self._maximum = maximum_value
         if not self._out_of_range_allowed:
             self.field_value.setMaximum(maximum_value)
+        self.updated_field()
+        self.update_slider()
 
     @property
     def value(self) -> float:
@@ -85,13 +96,8 @@ class FloatSliderWidget(QtWidgets.QWidget):
             new_value = min(max(new_value, self.minimum), self.maximum)
 
         self._value = new_value
-        self.field_value.setValue(new_value)
-        self.field_value.setToolTip(str(self.scaled_value))
-
-        if self._link_field_slider_range:
-            self.slider.current_value = self.scaled_value
-        else:
-            self.slider.current_value = new_value
+        self.updated_field()
+        self.update_slider()
 
     @property
     def scaled_value(self):
@@ -118,20 +124,14 @@ class FloatSliderWidget(QtWidgets.QWidget):
         self.field_value.setMinimum(float("-inf"))
         self.field_value.setMaximum(float("inf"))
 
-    def on_button_link_clicked(self):
-        self.button_link.setDown(self._link_field_slider_range)
-        self._link_field_slider_range = not self._link_field_slider_range
-
     def on_value_slider_changed(self):
         self._value = self.slider.current_value
-        self.field_value.setValue(self.value)
-        self.field_value.setToolTip(str(self.scaled_value))
+        self.updated_field()
         self.value_changed_signal.emit()
 
     def on_value_field_changed(self):
         self._value = self.field_value.value()
-        self.slider.current_value = self._value
-        self.field_value.setToolTip(str(self.scaled_value))
+        self.update_slider()
         self.value_changed_signal.emit()
 
     def set_display_color_range(self, color_range: list[tuple[int, QtGui.QColor]]):
@@ -143,3 +143,22 @@ class FloatSliderWidget(QtWidgets.QWidget):
             color_range: list[tuple(position, color)], where position goes from 0.0 to 1.0
         """
         self.slider.set_display_color_range(color_range=color_range)
+
+    def update_slider(self):
+        """
+        Update data displayed in the slider.
+        """
+        slider_value = self._value
+        if not self._out_of_range_allowed:
+            slider_value = remap(slider_value, self.minimum, self.maximum, 0.0, 1.0)
+        self.slider.current_value = slider_value
+
+    def updated_field(self):
+        """
+        Update data displayed in the value field.
+        """
+        field_value = self._value
+        if not self._out_of_range_allowed:
+            field_value = remap(field_value, 0.0, 1.0, self.minimum, self.maximum)
+
+        self.field_value.setValue(field_value)
