d143feec0b07de244f0c1cb97476165b63323e76
Liam Collod <lcollod@gmail.com>
2023-04-09 19:04:28 +0200

    [tests.editor...colorpicker] use custom stylesheet

--- tests/editor/__inviduals__/widgets_colorpicker_main.py
+++ tests/editor/__inviduals__/widgets_colorpicker_main.py
@@ -1,14 +1,23 @@
 import logging
 import sys
+from pathlib import Path
 
 from Qt import QtWidgets
 
 from gamutin.__main__ import _configureLogging
 from gamutin.editor.main import getQApp
 from gamutin.editor.testing import get_testing_window
+import gamutin.editor.widgets.colorpicker
 from gamutin.editor.widgets.colorpicker.main import ColorPickerWidget
 
 logger = logging.getLogger(__name__)
+
+
+def get_dev_stylesheet():
+    root = Path(gamutin.editor.widgets.colorpicker.__path__[0])
+    path = root / "stylesheet.css"
+    stylesheet = path.read_text()
+    return stylesheet
 
 
 def show():
@@ -19,6 +28,7 @@ def show():
     window.set_active_theme(window.themes.default)
     window.layout.setContentsMargins(0, 0, 0, 0)
     window.layout_user.setContentsMargins(0, 0, 0, 0)
+    window.setStyleSheet(get_dev_stylesheet())
 
     layout = QtWidgets.QVBoxLayout()
     layout.setContentsMargins(0, 0, 0, 0)
@@ -27,6 +37,7 @@ def show():
     layout.addWidget(widget)
 
     window.add_layout(layout)
+    window.resize(300, 450)
     window.show()
 
     sys.exit(app.exec_())
