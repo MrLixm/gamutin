90e3fd13235d8552dcb996be1df1328bd3ef3d36
Liam Collod <lcollod@gmail.com>
2023-05-13 20:50:51 +0200

    [editor...colorpicker...edit] colormanagement support

--- gamutin/editor/widgets/colorpicker/compounds/edit.py
+++ gamutin/editor/widgets/colorpicker/compounds/edit.py
@@ -1,6 +1,7 @@
 __all__ = ("ColorEditWidget",)
 
 import logging
+from typing import Optional
 
 from Qt import QtWidgets
 from Qt import QtCore
@@ -8,10 +9,9 @@ from Qt import QtCore
 from gamutin.core.color import LCHabColor
 from gamutin.editor.widgets.colorpicker.model import RGBAData
 from gamutin.editor.widgets.colorpicker.model import DEFAULT_COLOR
-from gamutin.editor.widgets.colorpicker.model import PASSTHROUGH_COLORSPACE
 from gamutin.editor.widgets.colorpicker.colormodel import ColorEditRgbModelWidget
 from gamutin.editor.widgets.colorpicker.colormodel import ColorEditLCHabModelWidget
-
+from gamutin.editor.widgets.colorpicker.model import GuiColorManagementControler
 
 logger = logging.getLogger(__name__)
 
@@ -23,6 +23,7 @@ class ColorEditWidget(QtWidgets.QWidget):
         super().__init__(parent)
 
         self._color = DEFAULT_COLOR
+        self._color_management = GuiColorManagementControler()
 
         # 1. Create
         self.layout = QtWidgets.QVBoxLayout()
@@ -42,16 +43,19 @@ class ColorEditWidget(QtWidgets.QWidget):
         self.tab_rgb.setObjectName("ColorEditRgbModelWidget")
 
         # 4. Connections
-        self.tab_rgb.color_changed_signal.connect(self.on_tab_rgb_changed)
+        self.tab_rgb.color_changed_signal.connect(self._on_tab_rgb_changed)
+        self.tab_lchab.color_changed_signal.connect(self._on_tab_lchab_changed)
         return
 
-    def set_color(self, color: RGBAData):
-        """
-        Set the color being edited, encoded in the given colorspace (might be None).
-        """
-        self._color = color
-        self.tab_rgb.set_color(color)
-        self.tab_lchab.set_color(LCHabColor.from_Rgba(color))
+    def _on_tab_rgb_changed(self):
+        self._color = self.tab_rgb.get_color()
+        self.color_changed_signal.emit()
+
+    def _on_tab_lchab_changed(self):
+        lchab_color = self.tab_lchab.get_color()
+        self._color = lchab_color.to_Rgba(
+            colorspace=self._color_management.working_colorspace
+        )
         self.color_changed_signal.emit()
 
     def get_color(self) -> RGBAData:
@@ -60,11 +64,25 @@ class ColorEditWidget(QtWidgets.QWidget):
         """
         return self._color
 
-    def on_tab_rgb_changed(self):
-        self._color = self.tab_rgb.get_color()
+    def set_color(self, color: RGBAData):
+        """
+        Set the color being edited, encoded in the given colorspace (might be None).
+        """
+        self._color = color
+        self.tab_rgb.set_color(color)
+        lchab_color = LCHabColor.from_Rgba(color)
+        self.tab_lchab.set_color(lchab_color)
         self.color_changed_signal.emit()
 
-    def on_tab_lchab_changed(self):
-        lchab_color = self.tab_lchab.get_color()
-        lchab_color.to_Rgba()
-        self.color_changed_signal.emit()
+    def get_color_management(self) -> GuiColorManagementControler:
+        """
+        Get the controller used to manage color-management.
+        """
+        return self._color_management
+
+    def set_color_management(self, color_management: GuiColorManagementControler):
+        """
+        Set the controller used to manage color-management.
+        """
+        self._color_management = color_management
+        self.tab_lchab.set_color_management(color_management)
